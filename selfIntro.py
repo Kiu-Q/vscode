ad3adaf1d55833b84d33304100456e0b77851838
Kiu-Q <78521221+Kiu-Q@users.noreply.github.com>
2022-06-13 09:59:14 +0800

    add self intro

---
+++ selfIntro.py
@@ -0,0 +1,20 @@
+text1='Name: Li Tsz Kiu, Thomas\nCurrent class level: Secondary 2'
+text2='Reason for joining this programme:'
+text3='1. Science ( S TEM)'
+text4='Interested in Chemistry'
+text5='2. Technology (S T EM)'
+text6='Familiar with Python (used in this video)'
+text7='3. Engineering  (ST E M)'
+text8='Tried to make a solar energy system'
+text9='Science or Engineering?'
+text10='Science'
+text11='Want to learn more on chemistry'
+text12='Ways to solve comunication problem:'
+text13='Video call or texting'
+text14='My technical background:'
+text15='Reached F4 chemistry\nPython and its packages\nWeb development'
+text16='This is the end\n Thank you!'
+text=[text1,text2,text3,text4,text5,text6,text7,text8,text9,text10,text11,text12,text13,text14,text15,text16]
+for i in text:
+    input()
+    print('>>> '+i)
